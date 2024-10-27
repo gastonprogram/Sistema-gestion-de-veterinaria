83c61ca2bea6c36cbe85fa91fdd5e6f4b689d7f6
gastonprogram <134446531+gastonprogram@users.noreply.github.com>
2024-10-27 15:04:54 -0300

    Add files via upload

--- Proyecto Veterinaria/turnos.py
+++ Proyecto Veterinaria/turnos.py
@@ -240,9 +240,6 @@ def modificarTurnoCliente(rutaArchivoTurnosProgramados, rutaArchivoClientesGuard
             # se encontro dni
             break
 
-
-
-
     # Filtrar los turnos que sean del cliente, que no esten cancelados y que sean posteriores a la fecha actual
 
     fecha_actual = datetime.now()
@@ -296,7 +293,7 @@ def modificarTurnoCliente(rutaArchivoTurnosProgramados, rutaArchivoClientesGuard
     fechaFinal = turnoSeleccionado["fecha"]
     especialistaFinal = turnoSeleccionado["documentoIdentidadEspecialista"]
     horarioFinal = turnoSeleccionado["horario"]
-    motivoFinal = ["motivo"]
+    motivoFinal = turnoSeleccionado["motivo"]
 
     
 
@@ -365,9 +362,6 @@ def modificarTurnoCliente(rutaArchivoTurnosProgramados, rutaArchivoClientesGuard
 
 
 
-            #
-            #      CCOOORREGIRRRR QUE NO GUARDE AL INSTANTE
-            #
             fechaFinal = nuevaFecha
             horarioFinal = horariosDisponibles[seleccion]
             print("Fecha y horario modificado correctamente.")
@@ -402,9 +396,6 @@ def modificarTurnoCliente(rutaArchivoTurnosProgramados, rutaArchivoClientesGuard
             while seleccion < 1 or seleccion > len(horariosDisponibles):
                 seleccion = int(input("Seleccione una opción válida: "))
 
-            #
-            #      CCOOORREGIRRRR QUE NO GUARDE AL INSTANTE
-            #
             horarioFinal = horariosDisponibles[seleccion]
 
             print("Horario modificado correctamente.")
@@ -450,10 +441,51 @@ def modificarTurnoCliente(rutaArchivoTurnosProgramados, rutaArchivoClientesGuard
 
             archivoProfesionalesLeer.close()
 
+            patronFecha = "^(0[1-9]|[12]\d|3[01])/(0[13578]|1[02])/(19\d{2}|20\d{2})$|^(0[1-9]|[12]\d|30)/(0[13456789]|1[012])/(19\d{2}|20\d{2})$|^(0[1-9]|1\d|2[0-8])/02/(19\d{2}|20\d{2})$|^29/02/(19([02468][048]|[13579][26])|20([02468][048]|[13579][26]))$"
+            
+            #seleccionar la fecha y verificar que este correcta
+            nuevaFecha = ""
+            while True:
+                nuevaFecha = input("Ingrese la nueva fecha (DD/MM/AAAA): ")
+                
+                if re.match(patronFecha, nuevaFecha):
+                    fecha_ingresada = datetime.strptime(nuevaFecha, "%d/%m/%Y")
+                    #fecha actual
+                    fecha_actual = datetime.now()
 
-            #
-            #       CORREGIRRRRRRRRRRRRRR HAY QUE CAMBIAR FECHA Y TAMBIEN HORARIO
-            #
+                    if fecha_ingresada > fecha_actual:
+                        break
+                    else:
+                        print("La fecha ingresada es anterior a la fecha actual. Intente de nuevo.")
+                else:
+                    print("El formato o la fecha es incorrecta. Intente de nuevo")
+
+
+            horarioInicio = int(horarios[:2])
+            horarioFin = int(horarios[8:10])
+
+            horariosDisponibles = [f"{hora:02d}:00" for hora in range(horarioInicio, horarioFin)]
+
+            #filtrar los horarios que ya estan ocupados
+            archivoTurnosLeer = open(rutaArchivoTurnosProgramados, "r", encoding="utf-8")
+            for turno in archivoTurnosLeer:
+                camposTurnos = turno.strip().split(";")
+                if camposTurnos[4] == nuevaFecha and camposTurnos[3] == turnoSeleccionado['documentoIdentidadEspecialista']:
+                    if camposTurnos[5] in horariosDisponibles:
+                        horariosDisponibles.remove(camposTurnos[5])
+
+            # Mostrar horarios disponibles
+            print("Horarios disponibles:")
+            for i, horario in enumerate(horariosDisponibles):
+                print(f"[{i}] {horario}")
+            
+            seleccion = int(input("Seleccione un nuevo horario: "))
+            while seleccion < 0 or seleccion > len(horariosDisponibles) - 1:
+                seleccion = int(input("Seleccione una opción válida: "))
+
+
+            fechaFinal = nuevaFecha
+            horarioFinal = horariosDisponibles[seleccion]
             especialistaFinal = nuevoEspecialista
 
             print("Especialista modificado correctamente.")
@@ -507,38 +539,78 @@ def guardarTurnoModificado(informacionTurnoModificado, rutaArchivoTurnosPrograma
     return
 
 
-def cancelarTurnoCliente(listTurnosProgramados, diccClientesGuardados):
+def cancelarTurnoCliente(rutaArchivoTurnosProgramados, rutaArchivoClientesGuardados):
 
-    documentoIdentidadCliente = input("DNI del cliente: ")
-    while documentoIdentidadCliente not in diccClientesGuardados.keys():
-        print("El DNI no se encuentra registrado.")
-        decision = input("Seleccione:\n[1] Ingresar DNI nuevamente\n[2] Regresar al menu\nElegir una opcion: ")
-        if decision == "1":
-            documentoIdentidadCliente = input("DNI del profesional: ")
-        elif decision == "2":
-            print("--------------------------------------------------------------------------------")
-            print("Volviendo al menu...")
-            print("--------------------------------------------------------------------------------")
-            #terminar el flujo y volver al menu
-            return
+    
+    archivoClientesLeer = open(rutaArchivoClientesGuardados, "r", encoding="utf-8")
+    
+    documentoIdentidadDueño = input("DNI del dueño: ")
+    while True:
+        encontrado = False
+
+        for registro in archivoClientesLeer:
+            campos = registro.strip().split(";")
+            documentoIdentidadRegistro = campos[0]
+
+            # Si el DNI está registrado
+            if documentoIdentidadDueño == documentoIdentidadRegistro:
+                encontrado = True
+                break
+
+        # Verificar si no se encontró el DNI después de recorrer el archivo
+        if not encontrado:
+            print("El DNI no se encuentra registrado.")
+            while True:
+                decision = input("Seleccione:\n[1] Ingresar DNI nuevamente\n[2] Regresar al menu\nElegir una opcion: ")
+                
+                if decision == "1":
+                    documentoIdentidadDueño = input("DNI del dueño: ")
+                    archivoClientesLeer.seek(0)  # Volver al inicio del archivo
+                    break
+                elif decision == "2":
+                    print("-" * 50)
+                    print("Volviendo al menu...")
+                    print("-" * 50)
+                    return
+                else:
+                    print("Ha seleccionado una opcion incorrecta.")
         else:
-            print("Ha seleccionado una opcion incorrecta.")
+            # se encontro dni
+            break
+    
     
     fecha_actual = datetime.now()
+    turnosCliente = []
 
-    # Filtrar los turnos que sean del cliente, que no esten cancelados y que sean posteriores a la fecha actual
-    turnosCliente = [
-    turno for turno in listTurnosProgramados 
-    if turno['documentoIdentidadCliente'] == documentoIdentidadCliente 
-    and turno["activo"] == True
-    and datetime.strptime(turno['fecha'], "%d/%m/%Y") >= fecha_actual
-    ]
+    archivoTurnosLeer = open(rutaArchivoTurnosProgramados, "r", encoding="utf-8")
+    for linea in archivoTurnosLeer:
+        campos = linea.strip().split(";")
+        turno = {
+            "pk": campos[0],
+            "documentoIdentidadCliente": campos[1],
+            "indiceMascota": campos[2],
+            "documentoIdentidadEspecialista": campos[3],
+            "fecha": campos[4],
+            "horario": campos[5],
+            "motivo": campos[6],
+            "activo": campos[7] == "True"
+        }
+
+        if (
+            turno["documentoIdentidadCliente"] == documentoIdentidadDueño and
+            turno["activo"] and
+            datetime.strptime(turno["fecha"], "%d/%m/%Y") >= fecha_actual
+        ):
+            turnosCliente.append(turno)
+
+    archivoTurnosLeer.close()
 
 
     if not turnosCliente:
         print("Este cliente no tiene turnos asignados.")
         return None
-
+    
+    
     #mostrar los turnos del cliente
     print("Turnos del cliente:")
     for i, turno in enumerate(turnosCliente):
@@ -550,16 +622,35 @@ def cancelarTurnoCliente(listTurnosProgramados, diccClientesGuardados):
         indiceTurno = int(input("Seleccione un índice válido: "))
 
     turnoSeleccionado = turnosCliente[indiceTurno]
-    indiceGeneralTurno = listTurnosProgramados.index(turnoSeleccionado)
 
     #confirmar si quieres cancelar el turno
     confirmarEliminacion = input("¿Está seguro que desea cancelar el turno (y/n)?: ").lower()
-    while confirmarEliminacion not in ["y", "n"]:
-        print("Valor incorrecto.")
+    while confirmarEliminacion != "y" and confirmarEliminacion != "n":
+        print("Ha ingresado un valor incorrecto.")
         confirmarEliminacion = input("¿Está seguro que desea cancelar el turno (y/n)?: ").lower()
 
     if confirmarEliminacion == "y":
-        listTurnosProgramados[indiceGeneralTurno]["activo"] = False
-        print("Se ha cancelado el turno con éxito.")
-    else:
-        print("Operación cancelada.")
+
+            archivoLeer = open(rutaArchivoTurnosProgramados, "r", encoding="utf-8")
+            lineas = archivoLeer.readlines()
+            archivoLeer.close()
+
+            archivoEscribir = open(rutaArchivoTurnosProgramados, "w", encoding="utf-8")
+            for linea in lineas:
+                campos = linea.strip().split(";")
+                if campos[0] == turnoSeleccionado["pk"]:
+                    archivoEscribir.write(f"{campos[0]};{campos[1]};{campos[2]};{campos[3]};{campos[4]};{campos[5]};{campos[6]};{False}\n")
+                else:
+                    archivoEscribir.write(linea)
+            archivoEscribir.close()
+
+            print("-" * 50)
+            print("Se ha cancelado el turno con éxito.")
+            print("-" * 50)
+
+    elif confirmarEliminacion == "n":
+        print("-" * 50)
+        print("Se ha elegido cancelar la operación.")
+        print("-" * 50)
+
+    return
